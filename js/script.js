f7a27adf9f70144027ceb18a148684676671b67e
FedeRios <riosferico@gmail.com>
2022-03-23 21:48:12 -0300

    Refresh contact link and fixed accopled views

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var twitterLink = 'https://twitter.com/FedeGRios';
 var linkedinLink = 'https://www.linkedin.com/in/federico-rios-a9130033';
 var githubLink = 'https://github.com/fedegrios';
+var resumeLink = 'fede_rios_resume.pdf';
 
 var btnTwitter = document.querySelector('.btn__twitter');
 btnTwitter.onclick = function(){
@@ -15,4 +16,9 @@ btnLinkedin.onclick = function(){
 var btnGithub = document.querySelector('.btn__github');
 btnGithub.onclick = function(){
     window.location.href = githubLink;
+}
+
+var btnGithub = document.querySelector('.btn__resume');
+btnGithub.onclick = function(){
+    window.location.href = resumeLink;
 }
